3db3353abf619c2a0f479102d156fd5f0d7d09d1
Hans Rinderknecht <hans.rinderknecht@gmail.com>
2023-10-01 17:25:50 -0400

    alpha.0 (initial push)
    This version always randomizes Ultima palette to a random palette.  There's an option at data\spells.py, line 129, that if set to "True" always selects from eight selected palettes.  However the default behavior is totally random.

--- data/spells.py
+++ data/spells.py
@@ -2,6 +2,7 @@ from data.spell import Spell
 from data.spell_names import id_name, name_id
 from data.ability_data import AbilityData
 from data.structures import DataArray
+from memory.space import Bank, Space, Reserve, Allocate, Free, Write, Read
 
 class Spells:
     BLACK_MAGIC_COUNT = 24
@@ -32,6 +33,7 @@ class Spells:
         for spell_index in range(len(self.name_data)):
             spell = Spell(spell_index, self.name_data[spell_index], self.ability_data[spell_index])
             self.spells.append(spell)
+
 
     def get_id(self, name):
         return name_id[name]
@@ -71,6 +73,65 @@ class Spells:
     def ultima_254_mp(self):
         ultima_id = name_id["Ultima"]
         self.spells[ultima_id].mp = 254
+
+    def ultima_random_color(self):
+        # Ultima spell animation color is controlled in 3 places:
+        # 1. Ultima palette at Battle Animation Palettes ($D26000) index 113 and index 110
+        #   --> Modifying palette 110 will affect other spells (Gem Dust, Storm)
+        # https://www.ff6hacking.com/wiki/doku.php?id=ff3:ff3us:doc:asm:list:battle_animation_palette
+        #
+        # 2. Ultima spell pointers to palettes in Attack Animation Data ($D07FB2):
+        # Ultima is spell index 20 in the attack animation data (data at $D0/80CA)
+        #   --> palette bg1 @ offset + 0x07 = $D0/80D1.  vanilla value = 110.
+        #   --> palette bg3 @ offset + 0x08 = $D0/80D2.  vanilla value = 113.
+        # https://www.ff6hacking.com/wiki/doku.php?id=ff3:ff3us:doc:asm:fmt:attack_animation_data
+        #
+        # 3. Change the color modifications in the Battle Animation Scripts ($D00000):
+        # Ultima bg1 is script pointer $00E7;  Ultima bg3 is script pointer $00E8.
+        # https://www.ff6hacking.com/wiki/doku.php?id=ff3:ff3us:doc:asm:codes:battle_animation_script
+        # Color appears to be in battle animation script, somewhere in here:
+        # D0/56CB: AF 60                    set background color subtraction to 0 (red)  <--
+        # D0/56CD: 89 0F                    loop start (15 times)
+        # D0/56CF: B6 C2                    increase background color subtraction by 2 (blue) <-- fade (r,g) from bg by 2 steps each time.
+        # D0/56D1: AE 42                    update (bg2) hdma scroll data (horizontal)
+        # D0/56D3: 00                       [$00]
+        # D0/56D4: 8A                       loop end
+        # D0/56D5: 89 7E                    loop start (126 times)
+        # D0/56D7: A3 16 31                 shift colors (1..7) of bg1 animation/esper palette left (3 loops per shift) <-- what does this do?
+        # D0/56DA: AE 42                    update (bg2) hdma scroll data (horizontal)
+        # D0/56DC: A6 00 00 01              move circle (+0,+0) and grow by 1
+        # D0/56E0: F7 C0                    wait until scanline 192
+        # D0/56E2: A7                       update circle
+        # D0/56E3: 00                       [$00]
+        # D0/56E4: 8A                       loop end
+
+        # Strategy should probably be:
+        # --> Identify several palettes that look good (red, orange, yellow, red, green, purple, black, white)
+        # --> choose one randomly
+        # --> set that palette in
+        # --> adjust the bg color subtraction accordingly.
+
+        # For now, let's just try randomly assigning the two palettes (range 0--239) and see what happens.
+        import random
+        ultima_palettes_addr = 0x1080D1
+        space = Reserve(ultima_palettes_addr, ultima_palettes_addr+1, "Randomize Ultima Palettes")
+        # Default values: 110, 113
+        palette_choices = [
+            [110, 113],  # blue (original)
+            [163, 211],  # red   - [210, 211] cometty
+            [223, 67],  # orange - 6 too red;  33 grey and orange;  [151, 75] is sparkly;  [128, 119] more muted
+            [126, 55],  # yellow - 63 very white; 81 a little orange; 55;  165 black & gold; 185 has blue flecks;  [20, 55],
+            [85, 49],    # green
+            [179, 131],  # purple
+            [94, 132],  # black/gray.  [88, 58] = black/yellow;  [155, 211] = black/red;
+            [63, 54]  # white
+        ]
+        if False:
+            new_palettes = random.choice(palette_choices)
+        else:
+            new_palettes = [random.randint(0, 239), random.randint(0, 239)]
+        space.write(new_palettes)
+        #print('Ultima palettes: ', new_palettes)
 
     def shuffle_mp(self):
         mp = []
@@ -112,6 +173,11 @@ class Spells:
         # Apply Ultima 254 MP after any MP shuffle/rando
         if self.args.ultima_254_mp:
             self.ultima_254_mp()
+
+        # Randomize Ultima color
+        if True:  # self.args.ultima_random_color:
+            self.ultima_random_color()
+
 
     def write(self):
         if self.args.spoiler_log:
